e85576095f9bb9738626ec83041048c64499c696
Ali-Elchab <alielchab01@gmail.com>
2023-12-31 11:25:12 +0200

    Initialized main and added expenses file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: //asdasd
-    ),
+    const MaterialApp(home: Expenses()),
   );
 }
