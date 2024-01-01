38cdf3af888a62c0ac2f277e8f6e1c263a775080
Ali-Elchab <alielchab01@gmail.com>
2024-01-01 11:42:25 +0200

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF326789));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        // ignore: deprecated_member_use
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+      ),
       home: const Expenses(),
     ),
   );
