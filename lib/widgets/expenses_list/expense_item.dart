9adb2f81993a4c1fde1ffdfc6ae1590c955a4708
Ali-Elchab <alielchab01@gmail.com>
2024-01-07 01:18:36 +0200

    Final commit

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,15 +1,15 @@
-import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
 
 class ExpenseItem extends StatelessWidget {
   const ExpenseItem(this.expense, {super.key});
 
   final Expense expense;
+
   @override
-  Widget build(context) {
-    //
+  Widget build(BuildContext context) {
     return Card(
-      color: Color.fromARGB(255, 74, 195, 255),
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 20,
@@ -20,19 +20,22 @@ class ExpenseItem extends StatelessWidget {
           children: [
             Text(
               expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
             ),
             const SizedBox(height: 4),
             Row(
               children: [
-                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}',
+                ),
                 const Spacer(),
                 Row(
                   children: [
                     Icon(categoryIcons[expense.category]),
-                    const SizedBox(width: 4),
+                    const SizedBox(width: 8),
                     Text(expense.formattedDate),
                   ],
-                )
+                ),
               ],
             ),
           ],
